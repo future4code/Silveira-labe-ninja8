90313009c7813fa07eb27df1e5566cf8f9f01bd5
arthurpraado <arthurbmprado@gmail.com>
2022-04-11 15:17:47 -0300

    Início projeto labeninjas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React from "react";
+import { AppContainer } from "./components/AppContainer";
+import Cabecalho from "./components/Cabecalho";
+import styled, { createGlobalStyle } from "styled-components";
+
+const EstiloGeral = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+}
+`;
 
 function App() {
-	return (
-        <AppContainer />
-	)
+  return (
+    <div>
+      <EstiloGeral />
+      {/* <AppContainer /> */}
+      <Cabecalho />
+    </div>
+  );
 }
 
-export default App
+export default App;
